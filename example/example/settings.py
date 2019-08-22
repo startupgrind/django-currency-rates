b8716b20164cf02a76c8717a9f1a8358059e475f
Alex Tsai <caffodian@users.noreply.github.com>
2019-08-22 15:50:39 -0700

    remove more deprecated stuff

--- example/example/settings.py
+++ example/example/settings.py
@@ -105,12 +105,6 @@ ROOT_URLCONF = 'example.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'example.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
