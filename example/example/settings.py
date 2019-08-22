d0f9ed041d4d652ecd4fc72708c77d1d7fc65dac
Alex Tsai <caffodian@users.noreply.github.com>
2019-08-22 15:50:06 -0700

    swap templates

--- example/example/settings.py
+++ example/example/settings.py
@@ -83,12 +83,12 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'he6k+bh)0el(ss9ta6--=59jk!4wo)&amp;m5(jzm7-mo0zx@w5d3z'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
 
 MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
