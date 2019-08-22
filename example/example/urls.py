b51b244d197fa874f1b64daf2b1a5d32a80a010d
Alex Tsai <caffodian@users.noreply.github.com>
2019-08-22 15:00:42 -0700

    new url(post 1.9) format

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
